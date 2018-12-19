27731c5853efa9381b87c00e883a6d95ecf090bd
Tania Whiting <tanwhiting@aol.com>
2018-12-19 02:35:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,11 +28,12 @@ class Owner
   end
   
   def buy_fish(name)
-    self.pets[:fishes] <<  Fish.new(name)
+    self.pets[:fishes] << Fish.new(name)
   end
   
-  def buy_cat
-end
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
+ end
 
   def buy_dog
 end
